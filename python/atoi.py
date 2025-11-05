35cc97ec24681214ad85a47499ef1b71c3595aab
nj7890 <43706707+nj7890@users.noreply.github.com>
2025-11-05 23:48:03 +0530

    Add myAtoi function for string to integer conversion
    Implement myAtoi function to convert string to integer.

---
+++ python/atoi.py
@@ -0,0 +1,28 @@
+def myAtoi(s):
+    sign = 1
+    res = 0
+    idx = 0
+    n = len(s)
+
+    while idx < n and s[idx] == ' ':
+        idx += 1
+
+    if idx < n and (s[idx] == '+' or s[idx] == '-'):
+        if s[idx] == '-':
+            sign = -1
+        idx += 1
+
+    while idx < n and '0' <= s[idx] <= '9':
+        digit = ord(s[idx]) - ord('0')
+        res = 10 * res + digit
+
+        if res > 2**31 - 1:
+            return (2**31 - 1) if sign == 1 else -2**31
+
+        idx += 1
+
+    return res * sign
+
+if __name__ == "__main__":
+    s = " -0012g4"
+    print(myAtoi(s))
